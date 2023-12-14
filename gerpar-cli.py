e0a82840798411d67caf0bbeecad402f5f1c11a0
pliniohrcode <pliniohrcode@gmail.com>
2023-12-14 11:02:55 -0300

    Implementa a versão cli de geração de parecer local e modifica os modelos de pareceres

--- gerpar-cli.py
+++ gerpar-cli.py
@@ -5,16 +5,15 @@ from openpyxl import load_workbook
 from gerpar import parecer_edital, parecer_contrato
 
 def make_parecer(data):
-    numero_contrato = data[0]
-    contratado = data[2]
-    cpf_cnpj = data[3]
-    objeto = data[4]
-    prazo_prorrogacao = data[5]
-    data_pedido_aditivo = data[6]
+    assunto = data[0]
+    processo = data[1]
+    num_modalidade = data[2]
+    dataEdital = data[3]
+    dataContrato = data[4]
 
     # São gerados os pareceres
-    parecer_edital(requerente, assunto, processo, num_modalidade, dataEdital)
-    parecer_contrato(requerente, assunto, processo, num_modalidade, dataContrato)
+    parecer_edital(assunto, processo, num_modalidade, dataEdital)
+    parecer_contrato(assunto, processo, num_modalidade, dataContrato)
 
 
 def main():
--- gerpar.py
+++ gerpar.py
@@ -4,7 +4,7 @@ from docx import Document
 # Para criar o parecer do edital
 
 
-def parecer_edital(requerente, assunto, processo, num_modalidade, data):
+def parecer_edital(assunto, processo, num_modalidade, data):
 
     # Abre o documento modelo de base
     with open("modelos/mp_edital.docx", "rb") as docFile:
@@ -13,38 +13,43 @@ def parecer_edital(requerente, assunto, processo, num_modalidade, data):
     # Laco for que pecorre o documento e realiza a adicao
     # das informacoes do novo parecer
     for paragraph in doc.paragraphs:
-        if "[REQUERENTE]" in paragraph.text:
-            orig_text = paragraph.text
-            new_text = str.replace(orig_text, "[REQUERENTE]", requerente)
-            paragraph.text = new_text
+    #     if "[REQUERENTE]" in paragraph.text:
+    #         orig_text = paragraph.text
+    #         new_text = str.replace(orig_text, "[REQUERENTE]", requerente)
+    #         paragraph.text = new_text
 
-        elif "[ASSUNTO]" in paragraph.text:
-            orig_text = paragraph.text
-            new_text = str.replace(orig_text, "[ASSUNTO]", assunto)
-            paragraph.text = new_text
-
-        elif "[PROCESSO_N]" in paragraph.text:
-            orig_text = paragraph.text
-            new_text = str.replace(orig_text, "[PROCESSO_N]", processo)
-            paragraph.text = new_text
+        if "[ASSUNTO]" in paragraph.text:
+            for run in paragraph.runs:
+                if "[ASSUNTO]" in run.text:
+                    run.text = run.text.replace("[ASSUNTO]", assunto)
+                    break
+            # orig_text = paragraph.text
+            # new_text = str.replace(orig_text, "[ASSUNTO]", assunto)
+            # paragraph.text = new_text
 
         elif "[MODALIDADE_N]" in paragraph.text:
             orig_text = paragraph.text
             new_text = str.replace(orig_text, "[MODALIDADE_N]", num_modalidade)
             paragraph.text = new_text
 
+        elif "[PROCESSO_N]" in paragraph.text:
+            orig_text = paragraph.text
+            new_text = str.replace(orig_text, "[PROCESSO_N]", processo)
+            paragraph.text = new_text
+      
         elif "[DATA]" in paragraph.text:
             orig_text = paragraph.text
             new_text = str.replace(orig_text, "[DATA]", data)
             paragraph.text = new_text
 
     # Salva o novo parecer criado
-    doc.save("pareceres/Parecer_Edital_%s.docx" % (processo))
+    nome_documento = str.replace(processo, '/', '-')
+    doc.save("pareceres/Parecer_Edital_%s.docx" % (nome_documento))
 
 # Para criar o parecer do contrato
 
 
-def parecer_contrato(requerente, assunto, processo, num_modalidade, data):
+def parecer_contrato(assunto, processo, num_modalidade, data):
 
     # Abre o documento modelo de base
     with open("modelos/mp_contrato.docx", "rb") as docFile:
@@ -53,24 +58,23 @@ def parecer_contrato(requerente, assunto, processo, num_modalidade, data):
     # Laco for que pecorre o documento e realiza a adicao
     # das informacoes do novo parecer
     for paragraph in doc.paragraphs:
-        if "[REQUERENTE]" in paragraph.text:
+    #     if "[REQUERENTE]" in paragraph.text:
+    #         orig_text = paragraph.text
+    #         new_text = str.replace(orig_text, "[REQUERENTE]", requerente)
+    #         paragraph.text = new_text
+        if "[ASSUNTO]" in paragraph.text:
+            for run in paragraph.runs:
+                if "[ASSUNTO]" in run.text:
+                    run.text = run.text.replace("[ASSUNTO]", assunto)
+                    break
+        elif "[MODALIDADE_N]" in paragraph.text:
             orig_text = paragraph.text
-            new_text = str.replace(orig_text, "[REQUERENTE]", requerente)
-            paragraph.text = new_text
-
-        elif "[ASSUNTO]" in paragraph.text:
-            orig_text = paragraph.text
-            new_text = str.replace(orig_text, "[ASSUNTO]", assunto)
+            new_text = str.replace(orig_text, "[MODALIDADE_N]", num_modalidade)
             paragraph.text = new_text
 
         elif "[PROCESSO_N]" in paragraph.text:
             orig_text = paragraph.text
             new_text = str.replace(orig_text, "[PROCESSO_N]", processo)
-            paragraph.text = new_text
-
-        elif "[MODALIDADE_N]" in paragraph.text:
-            orig_text = paragraph.text
-            new_text = str.replace(orig_text, "[MODALIDADE_N]", num_modalidade)
             paragraph.text = new_text
 
         elif "[DATA]" in paragraph.text:
@@ -79,4 +83,5 @@ def parecer_contrato(requerente, assunto, processo, num_modalidade, data):
             paragraph.text = new_text
 
     # Salva o novo parecer criado
-    doc.save("pareceres/Parecer_Contrato_%s.docx" % (processo))
+    nome_documento = str.replace(processo, '/', '-')
+    doc.save("pareceres/Parecer_Contrato_%s.docx" % (nome_documento))
